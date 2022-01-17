5d5a1bf290535673d86d8373d528c4ac2a6ede39
cbsandiego <cbuted@gmail.com>
2022-01-16 22:38:25 -0800

    cleaned adaboost data

--- crypto_signal_provider.py
+++ crypto_signal_provider.py
@@ -289,12 +289,11 @@ if option == 'Machine Learning Classifier - AdaBoost':
     st.subheader(f"AdaBoost")
     st.subheader(f"Selected Crypto:  {dropdown}")
     coin_yf_df = coin_list.drop(columns='Adj Close')
-    coin_yf_df_copy = coin_yf_df
     coin_yf_df.index=pd.to_datetime(coin_yf_df.index).date
     st.dataframe(coin_yf_df)
 
     # Filter the date index and close columns
-    coin_signals_df = coin_yf_df_copy.loc[:,["Close"]]
+    coin_signals_df = coin_yf_df.loc[:,["Close"]]
     
     # Use the pct_change function to generate returns from close prices
     coin_signals_df["Actual Returns"] = coin_signals_df["Close"].pct_change()
@@ -339,11 +338,18 @@ if option == 'Machine Learning Classifier - AdaBoost':
     # Create the target set selecting the Signal column and assigning it to y
     y = coin_signals_df["Signal"]
 
+    st.subheader("Training Model")
     # Select the start of the training period
+    #training_begin = st.date_input('Training Begin Date', value = pd.to_datetime('2020-01-01'))
+    st.caption(f'Training Begin Date starts at the selected "Start Date":  {start}')
     training_begin = X.index.min()
 
+
     # Select the ending period for the trianing data with an offet timeframe
-    training_end = X.index.min() + DateOffset(months=6)
+    #training_end = st.date_input('Training End Date', value = pd.to_datetime('2021-01-01'))
+    months = st.number_input("Enter number of months for DateOffset", 6)
+    training_end = X.index.min() + DateOffset(months=months)
+    st.caption(f'Training End Date ends:  {training_end}')
 
     # Generate the X_train and y_train DataFrame
     X_train = X.loc[training_begin:training_end]
@@ -379,13 +385,13 @@ if option == 'Machine Learning Classifier - AdaBoost':
     ab_y_pred = ab_model.predict(X_test)
 
     # Backtest the AdaBoost Model to evaluate performance
+    st.write('**AdaBoost Testing Classification Report**')
     ab_testing_report = classification_report(y_test,ab_y_pred)
 
     # Print the classification report
     st.write(ab_testing_report)
 
     # Create a new empty predictions DataFrame.
-
     # Create a predictions DataFrame
     alt_predictions_df = pd.DataFrame(index=X_test.index)
 
